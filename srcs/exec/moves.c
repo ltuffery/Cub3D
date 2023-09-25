0444643cfde75efef0eb9d34af2f5ffbc7fe08f9
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-09-25 17:04:36 +0200

    change rigth/left move && remove unused function

--- srcs/exec/moves.c
+++ srcs/exec/moves.c
@@ -6,17 +6,12 @@
 /*   By: ltuffery <ltuffery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:26:27 by ltuffery          #+#    #+#             */
-/*   Updated: 2023/09/25 15:48:37 by ltuffery         ###   ########.fr       */
+/*   Updated: 2023/09/25 17:03:50 by ltuffery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 #include <math.h>
-
-/*static int	is_wall(int player_y, int player_x, char **map)
-{
-	return (map[player_y][player_x] == '1');
-}*/
 
 void	move_forward(t_data *data)
 {
@@ -44,15 +39,17 @@ void	move_back(t_data *data)
 		data->player->y = tmpy;
 }
 
+#define TRUC 67.5
+
 void	move_rigth(t_data *data)
 {
 	float	tmpx;
 	float	tmpy;
 
-	tmpx = data->player->x + SPEED * cosf(data->player->direction + 90);
+	tmpx = data->player->x + SPEED * cosf(data->player->direction + TRUC);
 	if (data->map->content[(int)data->player->y][(int)tmpx] != '1')
 		data->player->x = tmpx;
-	tmpy = data->player->y + SPEED * sinf(data->player->direction + 90);
+	tmpy = data->player->y + SPEED * sinf(data->player->direction + TRUC);
 	if (data->map->content[(int)tmpy][(int)data->player->x] != '1')
 		data->player->y = tmpy;
 }
@@ -62,10 +59,10 @@ void	move_left(t_data *data)
 	float	tmpx;
 	float	tmpy;
 
-	tmpx = data->player->x - SPEED * cosf(data->player->direction + 90);
+	tmpx = data->player->x - SPEED * cosf(data->player->direction + TRUC);
 	if (data->map->content[(int)data->player->y][(int)tmpx] != '1')
 		data->player->x = tmpx;
-	tmpy = data->player->y - SPEED * sinf(data->player->direction + 90);
+	tmpy = data->player->y - SPEED * sinf(data->player->direction + TRUC);
 	if (data->map->content[(int)tmpy][(int)data->player->x] != '1')
 		data->player->y = tmpy;
 }
