f5e49e8fb1edb2cac999d0cebe948ca9225fe25a
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-09-26 11:58:16 +0200

    norm moves.c

--- srcs/exec/moves.c
+++ srcs/exec/moves.c
@@ -6,7 +6,7 @@
 /*   By: ltuffery <ltuffery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:26:27 by ltuffery          #+#    #+#             */
-/*   Updated: 2023/09/26 11:45:43 by ltuffery         ###   ########.fr       */
+/*   Updated: 2023/09/26 11:57:46 by ltuffery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ void	move_forward(t_data *data)
 {
 	float	tmpx;
 	float	tmpy;
+	float	rad;
 
-	tmpx = data->player->x + SPEED * cosf(data->player->direction->degree * PI / 180);
+	rad = data->player->direction->degree * PI / 180;
+	tmpx = data->player->x + SPEED * cosf(rad);
 	if (data->map->content[(int)data->player->y][(int)tmpx] != '1')
 		data->player->x = tmpx;
-	tmpy = data->player->y + SPEED * sinf(data->player->direction->degree * PI / 180);
+	tmpy = data->player->y + SPEED * sinf(rad);
 	if (data->map->content[(int)tmpy][(int)data->player->x] != '1')
 		data->player->y = tmpy;
 }
@@ -30,11 +32,13 @@ void	move_back(t_data *data)
 {
 	float	tmpx;
 	float	tmpy;
+	float	rad;
 
-	tmpx = data->player->x - SPEED * cosf(data->player->direction->degree * PI / 180);
+	rad = data->player->direction->degree * PI / 180;
+	tmpx = data->player->x - SPEED * cosf(rad);
 	if (data->map->content[(int)data->player->y][(int)tmpx] != '1')
 		data->player->x = tmpx;
-	tmpy = data->player->y - SPEED * sinf(data->player->direction->degree * PI / 180);
+	tmpy = data->player->y - SPEED * sinf(rad);
 	if (data->map->content[(int)tmpy][(int)data->player->x] != '1')
 		data->player->y = tmpy;
 }
@@ -45,11 +49,13 @@ void	move_rigth(t_data *data)
 {
 	float	tmpx;
 	float	tmpy;
+	float	rad;
 
-	tmpx = data->player->x + SPEED * cosf(data->player->direction->degree * PI / 180 + TRUC);
+	rad = data->player->direction->degree * PI / 180;
+	tmpx = data->player->x + SPEED * cosf(rad);
 	if (data->map->content[(int)data->player->y][(int)tmpx] != '1')
 		data->player->x = tmpx;
-	tmpy = data->player->y + SPEED * sinf(data->player->direction->degree * PI / 180 + TRUC);
+	tmpy = data->player->y + SPEED * sinf(rad);
 	if (data->map->content[(int)tmpy][(int)data->player->x] != '1')
 		data->player->y = tmpy;
 }
@@ -58,11 +64,13 @@ void	move_left(t_data *data)
 {
 	float	tmpx;
 	float	tmpy;
+	float	rad;
 
-	tmpx = data->player->x - SPEED * cosf(data->player->direction->degree * PI / 180 + TRUC);
+	rad = data->player->direction->degree * PI / 180;
+	tmpx = data->player->x - SPEED * cosf(rad);
 	if (data->map->content[(int)data->player->y][(int)tmpx] != '1')
 		data->player->x = tmpx;
-	tmpy = data->player->y - SPEED * sinf(data->player->direction->degree * PI / 180 + TRUC);
+	tmpy = data->player->y - SPEED * sinf(rad);
 	if (data->map->content[(int)tmpy][(int)data->player->x] != '1')
 		data->player->y = tmpy;
 }
