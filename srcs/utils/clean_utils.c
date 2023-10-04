eea1200e8b905352c23df4df24c007deb0be3f08
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-10-04 18:33:52 +0200

    Fix #24

--- srcs/utils/clean_utils.c
+++ srcs/utils/clean_utils.c
@@ -6,25 +6,35 @@
 /*   By: ltuffery <ltuffery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 16:37:49 by ltuffery          #+#    #+#             */
-/*   Updated: 2023/09/29 17:28:14 by ltuffery         ###   ########.fr       */
+/*   Updated: 2023/10/04 18:32:59 by ltuffery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MLX42/MLX42.h"
 #include "cub.h"
 #include <stdlib.h>
+
+static void	clean_player(t_player *player)
+{
+	if (player == NULL)
+		return ;
+	if (player->direction != NULL)
+		free(player->direction);
+	free(player);
+}
 
 void	clean_map(t_map *map)
 {
 	int	i;
 
 	if (map->no != NULL)
-		free(map->no);
+		mlx_delete_texture(map->no);
 	if (map->we != NULL)
-		free(map->we);
+		mlx_delete_texture(map->we);
 	if (map->ea != NULL)
-		free(map->ea);
+		mlx_delete_texture(map->ea);
 	if (map->so != NULL)
-		free(map->so);
+		mlx_delete_texture(map->so);
 	if (map->content != NULL)
 	{
 		i = 0;
@@ -35,6 +45,7 @@ void	clean_map(t_map *map)
 		}
 		free(map->content);
 	}
+	clean_player(map->player);
 }
 
 void	clean_rays(t_data *data)
@@ -54,6 +65,4 @@ void	clean_data(t_data *data)
 {
 	clean_map(data->map);
 	clean_rays(data);
-	free(data->player->direction);
-	free(data->player);
 }
