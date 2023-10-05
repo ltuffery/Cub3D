d2799e906d6631833479705bfcabd4e54d444cca
ltuffery <123221865+ltuffery@users.noreply.github.com>
2023-10-05 19:48:39 +0200

    test colision

--- srcs/exec/moves.c
+++ srcs/exec/moves.c
@@ -6,12 +6,20 @@
 /*   By: ltuffery <ltuffery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:26:27 by ltuffery          #+#    #+#             */
-/*   Updated: 2023/09/26 14:42:38 by ltuffery         ###   ########.fr       */
+/*   Updated: 2023/10/05 19:42:07 by ltuffery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 #include <math.h>
+#include <stdbool.h>
+
+static float	d_colision(float rad, bool cos)
+{
+	if (cos)
+		return (0.3 * cosf(rad));
+	return (0.3 * sinf(rad));
+}
 
 void	move(t_data *data, int pos1, int pos2)
 {
@@ -21,10 +29,10 @@ void	move(t_data *data, int pos1, int pos2)
 
 	rad = data->player->direction->degree * PI / 180;
 	tmpx = data->player->x + SPEED * pos1 * cosf(rad);
-	if (data->map->content[(int)data->player->y][(int)tmpx] != '1')
+	if (data->map->content[(int)(data->player->y + d_colision(rad, false) * pos2)][(int)(tmpx + d_colision(rad, true) * pos1)] != '1')
 		data->player->x = tmpx;
 	tmpy = data->player->y + SPEED * pos2 * sinf(rad);
-	if (data->map->content[(int)tmpy][(int)data->player->x] != '1')
+	if (data->map->content[(int)(tmpy + d_colision(rad, false) * pos2)][(int)(data->player->x + d_colision(rad, true) * pos1)] != '1')
 		data->player->y = tmpy;
 }
 
@@ -48,10 +56,10 @@ void	move_rigth(t_data *data)
 
 	rad = data->player->direction->degree * PI / 180;
 	tmpx = data->player->x - SPEED * cosf(rad - PI / 2);
-	if (data->map->content[(int)data->player->y][(int)tmpx] != '1')
+	if (data->map->content[(int)(data->player->y - d_colision(rad, false))][(int)(tmpx - d_colision(rad, true))] != '1')
 		data->player->x = tmpx;
 	tmpy = data->player->y - SPEED * sinf(rad - PI / 2);
-	if (data->map->content[(int)tmpy][(int)data->player->x] != '1')
+	if (data->map->content[(int)(tmpy - d_colision(rad, false))][(int)(data->player->x - d_colision(rad, true))] != '1')
 		data->player->y = tmpy;
 }
 
@@ -63,9 +71,9 @@ void	move_left(t_data *data)
 
 	rad = data->player->direction->degree * PI / 180;
 	tmpx = data->player->x + SPEED * cosf(rad - PI / 2);
-	if (data->map->content[(int)data->player->y][(int)tmpx] != '1')
+	if (data->map->content[(int)(data->player->y + d_colision(rad, false))][(int)(tmpx + d_colision(rad, true))] != '1')
 		data->player->x = tmpx;
 	tmpy = data->player->y + SPEED * sinf(rad - PI / 2);
-	if (data->map->content[(int)tmpy][(int)data->player->x] != '1')
+	if (data->map->content[(int)(tmpy + d_colision(rad, false))][(int)(data->player->x + d_colision(rad, true))] != '1')
 		data->player->y = tmpy;
 }
